83dfe4e4ed021b130e380d3e46b71fea264c9834
pedrobenevidesx <pedrohenriquevm215@gmail.com>
2026-01-31 16:16:33 -0300

    .

--- aulas/exerciciosAulas/04-ManipulandoRegex.js
+++ aulas/exerciciosAulas/04-ManipulandoRegex.js
@@ -7,19 +7,19 @@ const value = texto.replace(regex, "")
 console.log(value)
 */
 
-/*
+
 //Match - Procura padroes no texto e retorna o que foi encontrado
 const texto = "Tenho 2 gatos, 3 cachorros e 1 peixe"
 
-const regex = /\d+/g
+const regex = /\D+/g
 
 const value = texto.match(regex)
 
 console.log(value)
-*/
+
 
 // ------------------------------------------------
-
+/*
 //TEST - Responde se existe o padrao na string
 const texto = "Tenho 2 gatos, 3 cachorros e 1 peixe"
 
@@ -28,3 +28,4 @@ const regex = /\d+/g
 const value = regex.test(texto)
 
 console.log(value)
+*/
